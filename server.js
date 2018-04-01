e4c257434fb3c5c26b23c6dd989fe9aa91fbfefb
xcodemaker <dhammika.dsk@gmail.com>
2018-04-01 21:19:47 +0530

    heroku setup added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express=require('express'),
 hbs=require('hbs'),
 fs=require('fs');
 
+const port=process.env.PORT||3000;
+
 var app=express();
 
 hbs.registerPartials(__dirname+'/views/partials');
@@ -22,10 +24,11 @@ app.use((req,res,next)=>{
     next();
 });
 
-app.use((req,res,next)=>{
-    res.render('maintenance');
+// app.use((req,res,next)=>{
+//     res.render('maintenance');
+//     next();
 
-});
+// });
 
 app.use(express.static(__dirname+'/public'));
 
@@ -60,6 +63,6 @@ app.get('/bad',(req,res)=>{
         });
     });
 
-app.listen(3000,()=>{
-    console.log('server is up on port 3000.');
+app.listen(port,()=>{
+    console.log(`server is up on port ${port}.`);
 });
